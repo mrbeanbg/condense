309771317e81f774b616f9483734a2052fc0386c
Dinh Ngoc Anh <dinh.anh87@gmail.com>
2016-09-24 01:59:27 +0300

    BillingService calculating product transaction details

--- grails-app/controllers/condense/ProbaController.groovy
+++ grails-app/controllers/condense/ProbaController.groovy
@@ -7,11 +7,45 @@ class ProbaController {
 	BillingService billingService
 
 	def index() {
-		testPricingBook()
+//		def dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+//		print billingService.getDaysInMonth(dateFormat.parse("2015-02-01"))
+//		print billingService.getDaysInMonth(dateFormat.parse("2016-03-04"))
+//		print billingService.getDaysInMonth(dateFormat.parse("2016-04-04"))
+		//testPricingBook()
+		//testBillingPeriodsChunks()
+		geteffectivePricingPeriods()
+	}
+	
+	def geteffectivePricingPeriods() {
+		PricingBook.where { id > new Long(-1) }.deleteAll()
+		def dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+		new PricingBook(fromDate: dateFormat.parse("2015-02-01")).save()
+		new PricingBook(fromDate: dateFormat.parse("2015-05-01")).save()
+		new PricingBook(fromDate: dateFormat.parse("2015-05-11")).save()
+		new PricingBook(fromDate: dateFormat.parse("2015-05-15")).save()
+		new PricingBook(fromDate: dateFormat.parse("2015-05-16")).save()
+		new PricingBook(fromDate: dateFormat.parse("2015-06-01")).save()
+		new PricingBook(fromDate: dateFormat.parse("2015-06-15")).save()
+		
+		billingService.getBillingEffectivePeriods(dateFormat.parse("2015-02-10"), dateFormat.parse("2015-08-12")).each {print it}
+	}
+	def testBillingPeriodsChunks() {
+		def dateFormat = new SimpleDateFormat("yyyy-MM-dd")
+		billingService.getBillingPeriods(dateFormat.parse("2015-02-10"), dateFormat.parse("2015-08-12")).each {print it}
+		print "================"
+		billingService.getBillingPeriods(dateFormat.parse("2015-02-10"), dateFormat.parse("2015-08-12"), 30).each {print it}
+		print "================"
+		billingService.getBillingPeriods(dateFormat.parse("2015-02-10"), dateFormat.parse("2015-08-12"), null, 11).each {print it}
+		print "================"
+		billingService.getBillingPeriods(dateFormat.parse("2015-02-10"), dateFormat.parse("2015-08-12"), 30, 11).each {print it}
+		print "================"
+		billingService.getBillingPeriods(dateFormat.parse("2015-02-10"), dateFormat.parse("2015-08-12"), null, 5).each {print it}
+		print "================"
+		billingService.getBillingPeriods(dateFormat.parse("2015-02-10"), dateFormat.parse("2015-08-12"), 30, 5).each {print it}
 	}
 	
 	def testPricingBook() {
-		PricingBook.where { gt("id", new Long(-1)) }.deleteAll()
+		PricingBook.where { id > new Long(-1) }.deleteAll()
 		def dateFormat = new SimpleDateFormat("yyyy-MM-dd")
 		new PricingBook(fromDate: dateFormat.parse("2016-05-01")).save()
 		new PricingBook(fromDate: dateFormat.parse("2016-05-10")).save()
@@ -23,14 +57,15 @@ class ProbaController {
 		def foundCustomer = Customer.findByCspCustomerId("customer-123")
 		def customerSubscription = foundCustomer.subscriptions[0]
 		//print "BEFORE SEARCH ${customerSubscription}"
-		UsageRecord.where { gt("id", new Long(-1)) }.deleteAll()
+		UsageRecord.where { id > new Long(-1) }.deleteAll()
 		def usageDate = dateFormat.parse("2016-05-02")
-		new UsageRecord(startTime: usageDate, endTime: usageDate + 1, subscription: customerSubscription, quantity: 1).save()
-		new UsageRecord(startTime: usageDate, endTime: usageDate + 1, subscription: customerSubscription, quantity: 2).save()
-		new UsageRecord(startTime: usageDate, endTime: usageDate + 1, subscription: customerSubscription, quantity: 3).save()
+		new UsageRecord(startTime: usageDate, endTime: usageDate + 1, subscription: customerSubscription, quantity: 1, meteredId: "product A").save()
+		new UsageRecord(startTime: usageDate, endTime: usageDate + 1, subscription: customerSubscription, quantity: 2, meteredId: "product A").save()
+		new UsageRecord(startTime: usageDate, endTime: usageDate + 1, subscription: customerSubscription, quantity: 5, meteredId: "product B").save()
 		new UsageRecord(startTime: usageDate, endTime: usageDate + 1, subscription: foundCustomer.subscriptions[1], quantity: 4).save()
 		
 		billingService.getCustomerTransactions(foundCustomer, dateFormat.parse("2016-05-02"), dateFormat.parse("2016-05-16"))
+		//print billingService.getEffectivePricingBooks(dateFormat.parse("2016-05-01"), dateFormat.parse("2016-05-16"))
 	}
 	
     def testPricingEffectivePrice() {
--- grails-app/services/condense/BillingService.groovy
+++ grails-app/services/condense/BillingService.groovy
@@ -2,11 +2,37 @@ package condense
 
 import java.util.Date;
 
+import org.hibernate.criterion.CriteriaSpecification;
+
 import grails.transaction.Transactional
 
 @Transactional
 class BillingService {
-
+	Boolean isSimpleBillingPolicy
+	/**
+	 * Check if the billing policy is simple i.e. the last applied prices 
+	 * whose effective date is LESS OR EQUALS to the start of the billing period 
+	 * will be applied to the whole billing period.
+	 * 
+	 * Also, when calculating the price for a prorated period the included amount
+	 * will not also be prorated if the simple billing policy is used.
+	 * 
+	 * @return boolean
+	 */
+	def isSimpleBillingPolicy() {
+		return !!(isSimpleBillingPolicy)
+	}
+	
+	def checkBillingPeriodDates(Date fromDate, Date toDate, Integer billingPeriodDays = null) {
+		if (fromDate >= toDate) {
+			throw new Exception("Invalid billing period from ${fromDate} to ${toDate}")
+		}
+		def billingDay = fromDate[Calendar.DAY_OF_MONTH]
+		if (billingPeriodDays == null && billingDay == 29) {
+			throw new Exception ("Invalid billing day ${billingDay}. Can be only an integer between 1 and 28.")
+		}
+	}
+	
 	def getOriginalTier(PricingBook currentPricingBook, String productGuid, BigDecimal quantity) {
 		def matchedTier = currentPricingBook.tierDefinitions.findAll({
 			it.product.guid == productGuid && it.startQuantity <= quantity
@@ -28,11 +54,22 @@ class BillingService {
 		return productOverride
 	}
 	
+	/**
+	 * Find the appropriate effective price and included quantity for a product based on an usage amount
+	 * 
+	 * @param currentPricingBook the pricing book against which the tier definition will be defined
+	 * @param pricingSet the override policy that will be used
+	 * @param productGuid the GUID of a product 
+	 * @param quantity usage amount
+	 * 
+	 * @return the price to be charge, 
+	 * the amount of included (free) units for the whole billing period
+	 */
     def getEffectivePrice(PricingBook currentPricingBook, PricingSet pricingSet, String productGuid, BigDecimal quantity) {
-		print "Quantity: ${quantity}"
+		//print "Quantity: ${quantity}"
 		def originalTier = getOriginalTier(currentPricingBook, productGuid, quantity)
 		def price = originalTier.price
-		print "Original price: ${price}"
+		//print "Original price: ${price}"
 		def override = getEffectiveOverride(pricingSet, productGuid, quantity)
 
 		if (override != null) {
@@ -44,50 +81,290 @@ class BillingService {
 				price = override.amount
 			}
 		}
-		print "Override: ${override}"
-		def effectivePrice = ['effectivePrice': price, 'includedQuantity': originalTier.includedQuantity]
-		print effectivePrice
+		//print "Override: ${override}"
+		
+		def includedQuantity = override != null ? override.includedQuantity : originalTier.includedQuantity
+		def effectivePrice = ['price': price, 'includedQuantity': includedQuantity]
+		//print effectivePrice
 		return effectivePrice
     }
 	
+	/**
+	 * Get list of pricing book applied from the billing period sorted by the fromDate
+	 * @param fromDate the start of the billing period
+	 * @param toDate the end of the billing period
+	 * @return List of PricingBook
+	 */
 	def getEffectivePricingBooks(Date fromDate, Date toDate) {
-		def inPeriodPricingBooks = PricingBook.findAllByFromDateGreaterThanEqualsAndFromDateLessThan(fromDate, toDate)
-		def lastestPricingBookBeforePeriod = PricingBook.findAllByFromDateLessThan(fromDate).max {
-			it.fromDate
-		}
+		print PricingBook.list()
+		def inPeriodPricingBooks = PricingBook.findAllByFromDateGreaterThanEqualsAndFromDateLessThan(fromDate, toDate).sort {it.fromDate}
 		
-		assert lastestPricingBookBeforePeriod != null
-		
-		if (!inPeriodPricingBooks.contains(lastestPricingBookBeforePeriod)) {
-			inPeriodPricingBooks.add(lastestPricingBookBeforePeriod)
+		if (inPeriodPricingBooks.size() == 0 || inPeriodPricingBooks[0].fromDate != fromDate) {
+			// There isn't any  pricing book defined in period [fromDate, toDate). Get the latest pricing book before the billing period.
+			def lastestPricingBookBeforePeriod = PricingBook.findAllByFromDateLessThan(fromDate).max {
+				it.fromDate
+			}
+			if (lastestPricingBookBeforePeriod == null) {
+				throw new Exception ("Cannot find the first effective pricing book")
+			}
+			inPeriodPricingBooks.add(0, lastestPricingBookBeforePeriod)
 		}
-		return inPeriodPricingBooks.sort {it.fromDate}
+
+		return inPeriodPricingBooks
 	}
 	
-	def getProductUsages(Date fromDate, Date toDate) {
-		print "===== getProductUsages from ${fromDate} to ${toDate}"
-		return [["product": Product.find {guid == "0d1aa0ed-0a5d-4e42-ab7c-a12f55699b33"}, "usage": 100]]
+	/**
+	 * Split the period into chunks for which the customers is billed. 
+	 * It's considered that fromDate is always the start date of a billing period.
+	 * 
+	 * @param fromDate the beginning of the period
+	 * @param toDate the end of the period
+	 * @param billingPeriodDays the number of the days in the billing period. 
+	 * If omitted, it's considered that the customer is charged on the same day every month i.e. on the day of fromDate
+	 * 
+	 * @return billing period chunks
+	 */
+	def getBillingPeriods(Date fromDate, Date toDate, Integer billingPeriodDays = null) {
+
+		// Get the current month's billing date
+		def nextBillingDate
+
+		if (billingPeriodDays == null) {
+			nextBillingDate += getDaysInMonth(nextBillingDate)
+		} else {
+			nextBillingDate += billingPeriodDays
+		}
+		
+		def nextFromDate = fromDate
+		def billingPeriods = []
+		while (nextFromDate < toDate) {
+			nextBillingDate = [nextBillingDate, toDate].min()
+			billingPeriods << ["fromDate": nextFromDate, "toDate": nextBillingDate]
+			nextFromDate = nextBillingDate
+			if (nextFromDate < toDate) {
+				// Haven't reached toDate yet
+				nextFromDate = nextBillingDate
+				// Get the next billing date
+				if (billingPeriodDays == null) {
+					nextBillingDate += getDaysInMonth(nextBillingDate)
+				} else {
+					nextBillingDate += billingPeriodDays
+				}
+			}
+		}
+		return billingPeriods
 	}
 	
-	def getSubscriptionUsage(Subscription subscription, Date fromDate, Date toDate) {
+	/**
+	 * Get the effective pricing book for each billing period. 
+	 * If there are mid-term pricing changes aren't taken in consideration. 
+	 * The pricing book which is effective for the beginning of a billing period is applied for the whole term.
+	 * 
+	 * @param billingPeriods
+	 * @param effectivePricingBooks
+	 * @return
+	 */
+	def getSimpleEffectivePeriods(List billingPeriods, List effectivePricingBooks) {
+		def effectivePeriods = []
+		def currentPeriodIndex = 0
+		def currentPeriod = billingPeriods[currentPeriodIndex]
+		int i
+		for (i = 0; i < effectivePricingBooks.size() - 1; i++) {
+			while (currentPeriodIndex < billingPeriods.size() &&
+				effectivePricingBooks[i].fromDate <= currentPeriod.fromDate &&
+				effectivePricingBooks[i + 1].fromDate > currentPeriod.fromDate) {
+				effectivePeriods << ["fromDate": currentPeriod.fromDate, "toDate": currentPeriod.toDate, "pricingBook": effectivePricingBooks[i]]
+				currentPeriodIndex++
+				currentPeriod = billingPeriods[currentPeriodIndex]
+			}
+		}
+		
+		//Assign the last pricing book to the last billing period
+		while (currentPeriodIndex < billingPeriods.size()) {
+			currentPeriod = billingPeriods[currentPeriodIndex]
+			effectivePeriods << ["fromDate": currentPeriod.fromDate, "toDate": currentPeriod.toDate, "pricingBook": effectivePricingBooks[i]]
+			currentPeriodIndex++
+		}
+		return effectivePeriods
+	}
+	
+	def getComplexEffectivePeriods(List billingPeriods, List effectivePricingBooks) {
+		if (effectivePricingBooks.size() == 0) {
+			return []
+		}
+		
+		effectivePeriods = []
+		def pricingBookIndex = 0
+		billingPeriods.each {
+			def currentPricingBook = effectivePricingBooks[pricingBookIndex]
+			if (pricingBookIndex == effectivePricingBooks.size() - 1) {
+				// Reached the last pricing book i.e. this is the last pricing change and should be applicable the whole billing period
+				effectivePeriods << ["fromDate": it.fromDate, "toDate": it.toDate, "pricingBook": currentPricingBook]
+				
+			} else {
+				def nextPricingBook = effectivePricingBooks[pricingBookIndex + 1]
+				def fromDate = it.fromDate
+				while (pricingBookIndex < (effectivePricingBooks.size() - 1) && nextPricingBook.fromDate < it.toDate) {
+					effectivePeriods << ["fromDate": fromDate, "toDate": nextPricingBook.fromDate, "pricingBook": currentPricingBook]
+					fromDate = nextPricingBook.fromDate
+					currentPricingBook = nextPricingBook
+					pricingBookIndex++
+					nextPricingBook = effectivePricingBooks[pricingBookIndex + 1]
+				}
+				if (nextPricingBook.fromDate < it.toDate) {
+					// The last pricing book is reached. Split the rest of the billing period in 2
+					effectivePeriods << ["fromDate": fromDate, "toDate": nextPricingBook.fromDate, "pricingBook": currentPricingBook]
+					effectivePeriods << ["fromDate": nextPricingBook.fromDate, "toDate": toDate, "pricingBook": nextPricingBook]
+				}
+			}
+		}
+		return effectivePeriods
+	}
+	
+	/**
+	 * Get billing periods for which the price should be calculated according to the price list changes
+	 * 
+	 * @return list of billing periods with relevant effective pricing books 
+	 */
+	def getBillingEffectivePeriods(Date fromDate, Date toDate, Integer billingPeriodDays = null, Integer billingDay = null) {
+		def effectivePricingBooks = getEffectivePricingBooks(fromDate, toDate)
+		if (effectivePricingBooks.size() == 0) {
+			throw new Exception("No valid effecive pricing book found")
+		}
+		def billingEffectivePeriods = []
+		
+		def billingPeriods = getBillingPeriods(fromDate, toDate, billingPeriodDays)
+		if (isSimpleBillingPolicy()) {
+			return getSimpleEffectivePeriods(billingPeriods, effectivePricingBooks)
+		}
+		
+		return getComplexEffectivePeriods(billingPeriods, effectivePricingBooks)
+	}
+	
+	def getDaysInMonth(Date date) {
+		Calendar calendar = date.toCalendar();
+		return calendar.getActualMaximum(Calendar.DATE);		
+	}
+	
+	def getProductUsage(Product product, Date fromDate, Date toDate) {
+		if (billingPeriodDays == null) {
+			billingPeriodDays = getDaysInMonth(fromDate)
+		}
+		
+		assert (toDate - fromDate).days <= billingPeriodDays
+		
+		def totalUsage = UsageRecord.createCriteria().get {
+			and {
+				eq ("meteredId", product.guid)
+				ge ("startTime", fromDate)
+				lt ("startTime", toDate)
+			}
+			resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
+			projections {
+				sum("quantity", "totalUsage")
+			}
+		}.quantity
+		return usage
+	}
+	
+	def getProductTransactions(PricingSet pricingSet, Product product, List billingPeriods, List effectivePeriods, Integer billingPeriodDays = null) {
+		if (billingPeriods.size() == 0) {
+			return []
+		}
+		def transactions = []
+		billingPeriods.each {
+			def billingPeriodFromDate = it.fromDate
+			def billingPeriodToDate = it.toDate
+			def billingDays = (billingPeriodToDate - billingPeriodFromDate).days
+			
+			def billingEffectivePeriods = effectivePeriods.findAll { 
+				it.fromDate >= billingPeriodFromDate && it.toDate <= billingPeriodToDate}
+			
+			def effectivePeriodsDetails = []
+			def subTotal = 0
+			def totalUsage = 0
+			billingEffectivePeriods.each {
+				def usage = getProductUsage(product, it.fromDate, it.toDate)
+				
+				def effectivePrice = geteffectivePrice(it.pricingBook, pricingSet, product.guid, usage)
+				
+				def effectiveDays = (it.fromDate - it.toDate).days
+				
+				def includedForPeriod = (effectivePrice.includedQuantity *effectiveDays * 1.0) / billingDays
+				
+				//For better precision
+				def includedAmountForPeriod = (effectivePrice.includedQuantity * effectivePrice.price * effectiveDays * 1.0) / billingDays
+				
+				effectivePeriodsDetails << ["fromDate": it.fromDate, "toDate": it.toDate, 
+											"usage": usage, "price": effectivePrice.price, 
+											"included": effectivePrice.includedQuantity, "includedForPeriod": includedForPeriod]
+				subTotal += usage*effectivePrice.price - includedAmountForPeriod
+				totalUsage += usage
+			}
+			transactions << ["effectivePeriods": effectivePeriodsDetails,
+							 "fromDate": billingFromDate,
+							 "toDate": billingPeriodToDate,
+							 "subTotal": subTotal,
+							 "totalUsage": totalUsage]
+		}
+		
+	}
+	
+	def getSubscriptionTransactions(Subscription subscription, Date fromDate, Date toDate, Integer billingPeriodDays = null) {
+		checkBillingPeriodDates(fromDate, toDate, billingPeriodDays)
 		print "===== getSubscriptionUsage ${subscription}"
 		//assumed that the records are collected daily
 		def allUsages = UsageRecord.createCriteria().list {
 			and {
 				eq ("subscription.id", subscription.id)
-				le ("startTime", fromDate)
+				ge ("startTime", fromDate)
 				lt ("startTime", toDate)
 			}
+			resultTransformer(CriteriaSpecification.ALIAS_TO_ENTITY_MAP)
+			projections {
+				groupProperty("meteredId", "guid")
+				sum("quantity", "totalUsage")
+			}
 		}
-		allUsages.each {print it}
+		def subscriptionDetails = []
+		if (allUsages.size() > 0) {
+			def pricingSet = subscription.customer.pricingSet
+			
+			def billingPeriods = getBillingPeriods(fromDate, toDate, billingPeriodDays)
+			def effectivePeriods = getBillingEffectivePeriods(fromDate, toDate, billingPeriodDays)
+			
+			allUsages.each {
+				if (it.totalUsage > 0) {
+					def productGuid = it.guid
+					def product = Product.find {guid == productGuid}
+					def productDetails = getProductTransactions(pricingSet,
+						product, billingPeriods, effectivePeriods, billingPeriodDays)
+					subscriptionDetails << ["guid": productGuid, "details" : productDetails]
+				}
+			}
+		}
+		return subscriptionDetails
 	}
-	
-	def getCustomerTransactions(Customer customer, Date fromDate, Date toDate) {
-		def usageTransactions = []
+
+	/**
+	 * Get a summary of the generated consumptions for a given customer.
+	 * 
+	 * @param customer
+	 * @param fromDate always considered as the beginning of a billing period
+	 * @param toDate
+	 * @param billingPeriodDays the number of the days in the billing period. 
+	 * If omitted it's considered that the customer is charged on the same billing day every month. 
+	 * 
+	 * @return
+	 */
+	def getCustomerTransactions(Customer customer, Date fromDate, Date toDate, Integer billingPeriodDays = null) {
+		checkBillingPeriodDates(fromDate, toDate, billingPeriodDays)
+		def subscriptionDetails = []
 		customer.subscriptions.each{
-			usageTransactions << ["subscription": it, "usages": getSubscriptionUsage(it, fromDate, toDate)]
+			subscriptionDetails << ["subscription": it.subscriptionId, "details": getSubscriptionTransactions(it, fromDate, toDate, billingPeriodDays, billingDay)]
 		}
-		return usageTransactions
+		// TODO: return also the grand total and any other requested summary
+		return ["subscriptions": subscriptionDetails]
 	}
 
 }
